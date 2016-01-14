a5fe81944882cf472deacbedf7a645fbbeede2f5
root <root@magicbox.devel.acheron-services.net>
2016-01-14 12:50:01 +0100

    Fixed issue when creating Kafka topics. Swithed obp-full-kafka to develop branch

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -28,20 +28,26 @@ if [ ! -z "$LOG_RETENTION_BYTES" ]; then
 fi
 
 # Configure the default number of log partitions per topic
+PARTITIONS="--partition 1"
 if [ ! -z "$NUM_PARTITIONS" ]; then
     echo "default number of partition: $NUM_PARTITIONS"
     sed -r -i "s/(num.partitions)=(.*)/\1=$NUM_PARTITIONS/g" $KAFKA_HOME/config/server.properties
+    PARTITIONS="--partition $NUM_PARTITIONS"
 fi
 
+# Configure Kafka to atomatically create topics
+#echo "auto.create.topics.enable=true" >> $KAFKA_HOME/config/server.properties
+
 # Run Kafka
-$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties
+$KAFKA_HOME/bin/kafka-server-start.sh $KAFKA_HOME/config/server.properties &
 KAFKA_SERVER_PID=$!
 
 # Wait for Kafka to start completely 
 while netstat -lnt | awk '$4 ~ /:9092$/ {exit 1}'; do sleep 1; done
 
-# Create Request and Response topics
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partition 1 --topic "Request"
-$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 --partition 1 --topic "Response"
+# Create Request and Response topics 
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 $PARTITIONS --topic "Request"
+$KAFKA_HOME/bin/kafka-topics.sh --create --zookeeper localhost:2181 --replication-factor 1 $PARTITIONS --topic "Response"
  
 wait $KAFKA_SERVER_PID
+
