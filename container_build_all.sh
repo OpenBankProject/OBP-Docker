a89fdf8395e4737f1b826fb3857763c092776db6
tawoe <tobias.woelk@tesobe.com>
2019-12-11 16:06:50 +0100

    fresh rewrite - inital commit

--- container_build_all.sh
+++
@@ -1,3 +0,0 @@
-time docker build --no-cache -f Dockerfile.obp-base       -t openbankproject/obp-base .
-time docker build --no-cache -f Dockerfile.obp-full       -t openbankproject/obp-full .
-time docker build --no-cache -f Dockerfile.obp-full-kafka -t openbankproject/obp-full-kafka .
--- container_run_full.sh
+++
@@ -1,6 +0,0 @@
-docker run -d -p 8080-8082:8080-8082                   \
--e "OBP_API_HOSTNAME=http://127.0.0.1:8080"            \
--e "OBP_BASE_URL_API_EXPLORER=http://localhost:8082"   \
--e "OBP_BASE_URL_SOCIAL_FINANCE=http://localhost:8081" \
--e "OBP_WEBUI_API_EXPLORER_URL=http://localhost:8082"  \
-openbankproject/obp-full
